65d399d09ce6423d47998348b3e0ec076dc9d9dd
bennywilson2 <bennywilson@playfulcorp.com>
2019-05-03 22:28:56 -0500

    Re unwrapped the bus interior and seats.  Fixed skybox gradient.  Texture work

--- kbEngine/renderer/DX11/kbRenderer_DX11.cpp
+++ kbEngine/renderer/DX11/kbRenderer_DX11.cpp
@@ -720,7 +720,7 @@ void kbRenderer_DX11::Init_Internal( HWND hwnd, const int frameWidth, const int 
 
 	// sampler state
 	D3D11_SAMPLER_DESC samplerDesc;
-	samplerDesc.Filter = D3D11_FILTER_MIN_MAG_MIP_LINEAR;
+	samplerDesc.Filter = D3D11_FILTER_ANISOTROPIC;
 	samplerDesc.AddressU = D3D11_TEXTURE_ADDRESS_WRAP;
 	samplerDesc.AddressV = D3D11_TEXTURE_ADDRESS_WRAP;
 	samplerDesc.AddressW = D3D11_TEXTURE_ADDRESS_WRAP;
